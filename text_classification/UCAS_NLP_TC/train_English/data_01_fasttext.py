740df80bf4b58a670f5b3fb34375e505a58b80ee
759729997 <qiaoyongtian@qq.com>
2020-07-13 09:04:38 +0800

    0713

---
+++ text_classification/UCAS_NLP_TC/train_English/data_01_fasttext.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+"""训练数据与测试数据随机排序,百度细粒度分词结果
+"""
+import codecs
+import os
+import sys
+import json
+from tqdm import tqdm
+
+sys.path.insert(0, './')  # 定义搜索路径的优先顺序，序号从0开始，表示最大优先级
+
+import myClue  # noqa
+print('myClue module path :{}'.format(myClue.__file__))  # 输出测试模块文件位置
+from myClue.core import logger  # noqa
+from myClue.tools.file import read_file_texts  # noqa
+from myClue.tools.file import init_file_path  # noqa
+
+
+if __name__ == "__main__":
+    train_file_config = {
+        'train': './data/UCAS_NLP_TC/data_baidu_trans/train_trans.json',
+        'dev': './data/UCAS_NLP_TC/data_baidu_trans/dev_trans.json',
+        'test': './data/UCAS_NLP_TC/data_baidu_trans/test_trans.json',
+    }
+    output_path = './data/UCAS_NLP_TC/train_English/data_01_fasttext'
+    init_file_path(output_path)
+    for file_label, file_name in train_file_config.items():
+        logger.info('开始处理：{}'.format(file_label))
+        texts = read_file_texts(file_name)
+        output_file_name = os.path.join(output_path, '{}data.txt'.format(file_label))
+        with codecs.open(output_file_name, mode='w', encoding='utf8') as fw:
+            for text in tqdm(texts):
+                row_data = json.loads(text)
+                label = row_data['label']
+                cws_items = row_data['cws_items']
+                words = list()
+                for cws_item in cws_items:
+                    words.extend(cws_item['basic_words'])
+                news_content = ' '.join(words)
+                if len(news_content) == 0:
+                    continue
+                fw.write('{}\t{}\n'.format(label, news_content))
